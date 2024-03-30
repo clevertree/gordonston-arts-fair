f04bacda065aaeb516725f7591e2bf70e7cfdb2a
Ari Asulin <ari.asulin@gmail.com>
2024-03-29 21:26:45 -0400

    added hero. updated registration link

--- app/components/Hero/HeroContainer.tsx
+++ app/components/Hero/HeroContainer.tsx
@@ -1,14 +1,17 @@
 import styles from "./HeroContainer.module.css"
 import React from "react";
+import clsx from "clsx";
 
 interface HeroContainerProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    containerClassName: string,
+    className: string
 }
 
 export default function HeroContainer(props: HeroContainerProps) {
-    return <div className={styles.container}>
+    return <div className={clsx(styles.container, props.containerClassName)}>
         <div className={styles.background}></div>
-        <div className={styles.content}>
+        <div className={clsx(styles.content, props.className)}>
             {props.children}
         </div>
     </div>
