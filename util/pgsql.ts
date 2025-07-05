21291434f02bf5bebd63dbf5d5d802d4c05213ef
Ari Asulin <ari.asulin@phoenix.edu>
2025-07-05 13:18:51 -0400

    rename database env variable

--- util/pgsql.ts
+++ util/pgsql.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 
 export function getPGSQLClient() {
-  if (!process.env.PGSQL_DATABASE_URL) throw new Error('PGSQL_DATABASE_URL not set');
-  return neon(process.env.PGSQL_DATABASE_URL); // Or your direct connection string
+  if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not set');
+  return neon(process.env.DATABASE_URL); // Or your direct connection string
 }
